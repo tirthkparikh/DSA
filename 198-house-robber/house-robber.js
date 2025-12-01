c06a9fc985b218931c7bf5370a6358e8f7a83b0c
TIRTH PARIKH <tirthkparikh@gmail.com>
2025-12-01 11:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (72.68%) - LeetSync

---
+++ 198-house-robber/house-robber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    let dp = [];
+    dp[0] = nums[0];
+    dp[1] = Math.max(dp[0],nums[1]);
+    for(let i = 2; i <= nums.length; i++){
+        dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+    }
+    return dp[nums.length-1];
+};
